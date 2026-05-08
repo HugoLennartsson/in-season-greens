5f63472de7dc82ddd4fde85cdda7625b0aa4418d
jakomons <jakob.asp@hotmail.se>
2026-05-09 01:07:54 +0200

    api key är inte public längre :)

--- API/API_nutritions.py
+++ API/API_nutritions.py
@@ -1,9 +1,12 @@
+import os
+
 import requests
 from urllib.request import urlopen
 import json
+from dotenv import load_dotenv
 api_url = 'https://api.calorieninjas.com/v1/nutrition?query='
 
-
+load_dotenv("key.env")
 with open('all_produce.json') as f:
     d = json.load(f)
     
@@ -11,7 +14,7 @@ for i in d:
     if "name_en" in i:  
         
         query = i["name_en"]
-        response = requests.get(api_url + query, headers={'X-Api-Key': 'ayNXvPJGIyjZ2B3WhlJzxQ==XicV8IFlPMOsogPx'})
+        response = requests.get(api_url + query, headers={'X-Api-Key': os.getenv("api_key")})
         response2 = response.json()["items"]
         
         for item in response2:
